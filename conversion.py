790a9d99e2fc81544b1819fbd24191ebbcaeac47
VinayakPrakashh <vinayakprakash2121@gmail.com>
2023-09-19 21:55:48 +0530

    hh

---
+++ conversion.py
@@ -0,0 +1,4 @@
+print("DECIMAL TO BINARY CONVERSION")
+x=int(input("ENTER THE DECIMAL NUMBER: "))
+binary_num = bin(x)
+print(f"The binary representation of {x} is {binary_num[2:]}")
---
+++ sample.py
@@ -0,0 +1,7 @@
+a=int(input("enter a= "))
+b=int(input("enter b= "))
+c=int(input("enter c= "))
+d=(b*b)-(4*a*c)
+x=((-b)-(d**0.5))/2*a
+y=((-b)+(d**0.5))/2*a
+print("roota are: ",x,y)
